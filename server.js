e953828c5954f89c130d0f814dc05d996b286c70
Harish <harishbala1o@gmail.com>
2021-08-10 23:30:13 +0530

    HTML file added

--- server.js
+++ server.js
@@ -1,28 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require('path');
 require("dotenv").config();
 
 const restaurantRouter = require("./routes/api");
-
-const http = require ('http');
-const fs  = require ('fs');
-
-const handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
-
 
 // Setup server port
 const port = process.env.PORT || 4000;
@@ -43,7 +25,7 @@ app.use("/restaurants", restaurantRouter);
 
 // define a root/default route
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+  res.sendFile(path.join (__dirname+'/index.html'));
 });
 // listen for requests
 app.listen(port, () => {
